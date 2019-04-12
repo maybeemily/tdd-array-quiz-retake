a6c8286096dfeb02d2edb1881e3a61447a9f67ca
emily baier <emilyrbaier@gmail.com>
2019-04-12 15:13:23 -0700

    function written, test passed

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+const { bigNumbers } = require('../lib/index');
+
+describe('BIG test', ()=> {
+    it('remove numbers less than 5 and modify to phrase', ()=> {
+        const numbers = [1, 4, 5, 15, 3, 12, 7];
+        const result = bigNumbers(numbers);
+        expect(result).toEqual(['Wow, 5 is big!', 'Wow, 15 is big!', 'Wow, 12 is big!', 'Wow, 7 is big!']);
+    });
+});
