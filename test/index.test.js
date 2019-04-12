d435742810a608b8313ee144fe61f7ba7a03abb2
emily baier <emilyrbaier@gmail.com>
2019-04-12 15:15:18 -0700

    clarified test description

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 const { bigNumbers } = require('../lib/index');
 
 describe('BIG test', ()=> {
-    it('remove numbers less than 5 and modify to phrase', ()=> {
+    it('remove numbers less than 5 and modify phrase', ()=> {
         const numbers = [1, 4, 5, 15, 3, 12, 7];
         const result = bigNumbers(numbers);
         expect(result).toEqual(['Wow, 5 is big!', 'Wow, 15 is big!', 'Wow, 12 is big!', 'Wow, 7 is big!']);
